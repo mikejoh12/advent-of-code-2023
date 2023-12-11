90f357589815682aac0d42d8a50730bbb471e377
Michael Johansson <mikejoh12@gmail.com>
2023-12-11 14:48:59 -0600

    Combine day 11 part 1 and 2 into one project

--- rust/day-11/src/main.rs
+++ rust/day-11/src/main.rs
@@ -1,69 +1,73 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
 use itertools::Itertools;
+use std::fs;
+use std::vec;
 
-#[derive(Debug)]
 struct Galaxy {
-    id: usize,
     x: usize,
     y: usize,
 }
 
-#[derive(Debug)]
 struct Space {
     map: Vec<Vec<char>>,
     galaxies: Vec<Galaxy>,
+    galaxies_expanded: Vec<Galaxy>,
+    row_expansions: Vec<usize>,
+    col_expansions: Vec<usize>,
 }
 
 impl Space {
-    fn expand_vert(&mut self) {
-        let mut expanded: Vec<Vec<char>> = vec![];
-        for row in self.map.iter() {
-            if !row.contains(&'#') {
-                for _ in 0..1_000_000 {
-                    expanded.push(row.to_vec());
-                }
-            }
-            expanded.push(row.to_vec());
-        }
-        self.map = expanded;
-    }
-
-    fn expand_hor(&mut self) {
-        let mut expanded: Vec<Vec<char>> = vec![vec![];self.map.len()];
-        for i in 0..self.map.first().expect("Map row should not be empty").len() {
-            if self.map.iter().all(|r|r.iter().nth(i).unwrap() != &'#') {
-                for _ in 0..1_000_000 {
-                    for j in 0..expanded.len()  {
-                        expanded[j].push('.');
-                    }
-                }
-            }
-            for j in 0..expanded.len()  {
-                expanded[j].push(self.map[j][i]);
-            }
-        }
-        self.map = expanded;
-    }
-
     fn add_galaxies(&mut self) {
         let mut galaxies: Vec<Galaxy> = Vec::new();
-        let mut id: usize = 1;
         for (y, row) in self.map.iter().enumerate() {
             for (x, c) in row.iter().enumerate() {
                 if *c == '#' {
-                    galaxies.push(Galaxy { id, x, y});
-                    id += 1;
+                    galaxies.push(Galaxy { x, y });
                 }
             }
         }
         self.galaxies = galaxies;
     }
 
+    fn find_expansions(&mut self) {
+        for (y, row) in self.map.iter().enumerate() {
+            if !row.contains(&'#') {
+                self.row_expansions.push(y);
+            }
+        }
+
+        for x in 0..self.map.first().expect("Map row should not be empty").len() {
+            if self.map.iter().all(|r| r.iter().nth(x).unwrap() != &'#') {
+                self.col_expansions.push(x);
+            }
+        }
+    }
+
+    fn expand(&mut self, n: usize) {
+        let mut galaxies: Vec<Galaxy> = vec![];
+        for galaxy in &self.galaxies {
+            let add_rows = self
+                .row_expansions
+                .iter()
+                .filter(|&y| y < &galaxy.y)
+                .count()
+                * (n - 1);
+            let add_cols = self
+                .col_expansions
+                .iter()
+                .filter(|&x| x < &galaxy.x)
+                .count()
+                * (n - 1);
+            galaxies.push(Galaxy {
+                x: galaxy.x + add_cols,
+                y: galaxy.y + add_rows,
+            })
+        }
+        self.galaxies_expanded = galaxies
+    }
+
     fn get_distances(&self) -> usize {
         let mut length_sum = 0;
-        for pair in self.galaxies.iter().combinations(2) {
+        for pair in self.galaxies_expanded.iter().combinations(2) {
             let x_diff = pair[0].x.max(pair[1].x) - pair[0].x.min(pair[1].x);
             let y_diff = pair[0].y.max(pair[1].y) - pair[0].y.min(pair[1].y);
             length_sum += x_diff + y_diff;
@@ -73,25 +77,24 @@ impl Space {
 }
 
 fn main() {
-    let mut map: Vec<Vec<char>> = vec![];
-    if let Ok(lines) = read_lines("./input.txt") {
-        for line in lines {
-            if let Ok(ip) = line {
-                let row: Vec<char> = ip.chars().collect();
-                map.push(row);
-            }
-        }
-    }
-    let mut space: Space = Space{ map, galaxies: vec![] };
-    space.expand_vert();
-    space.expand_hor();
+    let map: Vec<Vec<char>> = fs::read_to_string("input.txt")
+        .expect("File should open")
+        .split("\n")
+        .map(|r| r.chars().collect())
+        .collect();
+    let mut space = Space {
+        map,
+        galaxies: vec![],
+        galaxies_expanded: vec![],
+        row_expansions: vec![],
+        col_expansions: vec![],
+    };
     space.add_galaxies();
-    //println!("Space {:?}", space);
-    println!("Part 1: {}", space.get_distances());
-}
+    space.find_expansions();
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    space.expand(2);
+    println!("Part 1: {}", space.get_distances());
+
+    space.expand(1_000_000);
+    println!("Part 2: {}", space.get_distances());
 }
